ea2b4f56a4e9833ef9b6940d3d19364fcc66e93c
RodrigoNdeC <rodrigondec@gmail.com>
2018-06-28 16:31:14 -0300

    views e componentes adicionados

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+
+import HomeView from '@/views/HomeView'
+import SobreView from '@/views/SobreView'
+import HelloWorld from '@/components/HelloWorld'
+import CardList from '@/components/CardList'
 
 Vue.use(Router)
 
 export default new Router({
-    routes: [
-        {
-            path: '/',
-            name: 'Hello',
-            component: Hello
-        }
-    ]
+	routes: [
+		{
+			path: '/',
+			name: 'HomeView',
+			component: HomeView
+		},
+		{
+			path: '/content',
+			name: 'CardList',
+			component: CardList
+		},
+		{
+			path: '/sobre',
+			name: 'SobreView',
+			component: SobreView
+		},
+		{
+			path: '/hello',
+			name: 'HelloView',
+			component: HelloWorld
+		}
+	]
 })
