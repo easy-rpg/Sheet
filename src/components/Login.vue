02e57a6d4cccefc0d48422906d2121ac729fe638
RodrigoNdeC <rodrigondec@gmail.com>
2018-07-01 16:53:09 -0300

    ok

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -41,13 +41,9 @@ export default {
     },
     methods: {
         submit() {
-            var credentials = {
-                username: this.credentials.username,
-                password: this.credentials.password
-            }
             // We need to pass the component's this context
             // to properly make use of http in the auth service
-            auth.login(this, credentials, '/')
+            auth.login(this, this.credentials, '/')
         }
     }
 }
