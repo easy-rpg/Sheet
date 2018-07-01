8ec6d9428d3fe4602135911bf0b5c31b30059308
RodrigoNdeC <rodrigondec@gmail.com>
2018-07-01 13:57:29 -0300

    userbar adicionado

---
+++ src/components/Userbar.vue
@@ -0,0 +1,43 @@
+<template lang="html">
+    <v-menu v-if="username" :nudge-width="100">
+        <v-toolbar-title slot="activator">
+            <v-btn flat>
+                <span>{{ username }}&nbsp;</span>
+                <v-icon>fas fa-caret-down</v-icon>
+            </v-btn>
+        </v-toolbar-title>
+        <v-list>
+            <v-list-tile key="Perfil" to="/">
+                <v-list-tile-action>
+                    <v-icon>fas fa-user</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title>Perfil</v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
+            <v-divider ></v-divider>
+            <v-list-tile key="Sair" @click="logout">
+                <v-list-tile-action>
+                    <v-icon>fas fa-sign-out-alt</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title>Sair</v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
+    </v-menu>
+</template>
+
+<script>
+export default {
+    name: 'userbar',
+    data (){
+        return {
+            username: ''
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
