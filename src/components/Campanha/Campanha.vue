dac3304ed4b2f133667a4999d86b7d3a74b92186
RodrigoNdeC <rodrigondec@gmail.com>
2018-07-04 19:56:55 -0300

    Esqueletos dos componentes adicionados

---
+++ src/components/Campanha/Campanha.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+
+</template>
+
+<script>
+export default {
+    data () {
+        return { }
+    },
+    beforeCreate: function () {
+        if (!this.$session.exists()) {
+            this.$router.push('/login')
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
