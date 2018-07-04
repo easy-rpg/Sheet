550070f04d6a1ef0bde415d5bcffea9ef86a558a
RodrigoNdeC <rodrigondec@gmail.com>
2018-07-04 01:51:34 -0300

    signup = register

--- src/auth/index.js
+++ src/auth/index.js
@@ -38,19 +38,34 @@ export default {
         })
     },
 
-    signup(context, creds, redirect) {
-        context.$http.post(SIGNUP_URL, creds, (data) => {
-            localStorage.setItem('id_token', data.id_token)
-            localStorage.setItem('access_token', data.access_token)
-
-            this.user.authenticated = true
-
-            if(redirect) {
-                router.go(redirect)
+    register(context, creds, redirect) {
+        context.$http.post(REGISTER_URL, creds).then(function (response) {
+            if (response.status === 200) {
+                context.$session.destroy()
+                context.$session.start()
+                context.$session.set('username', response.data.username)
+                context.$session.set('id_user', response.data.id_user)
             }
-
-        }).error((err) => {
-            context.error = err
+            // return context.$http
+        }, function (err) {
+            console.log('err', err)
+        }).then(function (){
+            context.$http.get(AUTH_URL, {'username': creds.username, 'password': creds.password}).then(function (response) {
+                if (response.status === 200) {
+                    context.$session.set('jwt', response.data.token)
+                    axios.defaults.headers.common['Authorization'] = 'JWT ' + response.data.token
+                }
+                // Redirect to a specified route
+                if(redirect) {
+                    context.$router.push(redirect)
+                }
+            // return context.$http
+            }, function (err) {
+                console.log('err', err)
+            }).then(function (){
+                console.log({'id_user': context.$session.get('id_user'), 'username': context.$session.get('username'), 'jwt': context.$session.get('jwt')})
+                // return context.$http
+            })
         })
     }
 }
