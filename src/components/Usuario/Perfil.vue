55edfa70aecfad5b5504cb53d504dd8a10d6ef71
Pedro Coelho <pedro_coelho_pv@hotmail.com>
2018-07-04 23:11:12 -0300

    Add the initial template to profile page

--- src/components/Usuario/Perfil.vue
+++ src/components/Usuario/Perfil.vue
@@ -4,9 +4,34 @@
             <v-layout row wrap align-center justify-center>
                 <v-flex xs12 md4 sm8>
                     <v-card class="elevation-12">
-                        <v-container fluid grid-list-lg>
+                        <v-card color="black" class="white--text">
+                            <v-container fill-height fluid>
+                                <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline">Configurações de perfil</span>
+                                </v-flex>
+                                </v-layout>
+                            </v-container>
+                        </v-card>
+                        <v-container fluid>
                             <v-layout row>
-                                <v-flex >
+                                    <v-flex xs5 md12 sm12>
+                                        <v-text-field label="Usuário" v-model="user.username">
+                                        </v-text-field>
+                                    </v-flex>
+                                    <v-flex xs7 md12 sm12>
+                                        <v-text-field label="Email" v-model="user.email">
+                                        </v-text-field>
+                                    </v-flex>
+                            </v-layout>
+                            <v-layout row>
+                                <v-flex xs5 md12 sm12>
+                                    <v-text-field label="Primeiro nome" v-model="user.first_name">
+                                    </v-text-field>
+                                </v-flex>
+                                <v-flex xs7 md12 sm12>
+                                    <v-text-field label="Último nome" v-model="user.last_name">
+                                    </v-text-field>
                                 </v-flex>
                             </v-layout>
                         </v-container>
