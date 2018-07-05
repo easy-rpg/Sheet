a0730332d233377161432b0447e1ba49057aa928
Pedro Coelho <pedro_coelho_pv@hotmail.com>
2018-07-05 00:34:32 -0300

    - Splitting Perfil in PerfilCard and ChangePass components

---
+++ src/components/Usuario/ChangePass.vue
@@ -0,0 +1,60 @@
+<template>
+	<v-card class="elevation-12">
+	    <v-card color="black" class="white--text">
+	        <v-container fill-height fluid>
+	            <v-layout fill-height>
+	            <v-flex xs12 align-end flexbox>
+	                <span class="headline">Alterar senha</span>
+	            </v-flex>
+	            </v-layout>
+	        </v-container>
+	    </v-card>
+	    <v-container fluid>
+	        <v-layout row wrap>
+	                <v-flex xs4 md12 sm12>
+	                    <v-text-field label="Senha antiga">
+	                    </v-text-field>
+	                </v-flex>
+		            <v-flex xs4 md12 sm12>
+		                <v-text-field label="Nova senha">
+		                </v-text-field>
+		            </v-flex>
+		            <v-flex xs4 md12 sm12>
+		                <v-text-field label="Confirmar senha">
+		                </v-text-field>
+		            </v-flex>
+	        </v-layout>
+	    </v-container>
+	    <v-card-actions>
+	         <v-btn
+	                absolute
+	                dark
+	                fab
+	                bottom
+	                right
+	                color="primary"
+	                slot="activator"
+	                @click="update"
+	            >
+	                <v-icon>done</v-icon>
+	            </v-btn>
+	    </v-card-actions>
+	</v-card>
+</template>
+<script>
+export default {
+	data () {
+		return {
+
+		}
+	},
+	mounted () {
+
+	},
+	methods: {
+		update () {
+			
+		}
+	}
+}	
+</script>
--- src/components/Usuario/Perfil.vue
+++ src/components/Usuario/Perfil.vue
@@ -3,39 +3,10 @@
         <v-container fluid grid-list-lg>
             <v-layout row wrap align-center justify-center>
                 <v-flex xs12 md4 sm8>
-                    <v-card class="elevation-12">
-                        <v-card color="black" class="white--text">
-                            <v-container fill-height fluid>
-                                <v-layout fill-height>
-                                <v-flex xs12 align-end flexbox>
-                                    <span class="headline">Configurações de perfil</span>
-                                </v-flex>
-                                </v-layout>
-                            </v-container>
-                        </v-card>
-                        <v-container fluid>
-                            <v-layout row>
-                                    <v-flex xs5 md12 sm12>
-                                        <v-text-field label="Usuário" v-model="user.username">
-                                        </v-text-field>
-                                    </v-flex>
-                                    <v-flex xs7 md12 sm12>
-                                        <v-text-field label="Email" v-model="user.email">
-                                        </v-text-field>
-                                    </v-flex>
-                            </v-layout>
-                            <v-layout row>
-                                <v-flex xs5 md12 sm12>
-                                    <v-text-field label="Primeiro nome" v-model="user.first_name">
-                                    </v-text-field>
-                                </v-flex>
-                                <v-flex xs7 md12 sm12>
-                                    <v-text-field label="Último nome" v-model="user.last_name">
-                                    </v-text-field>
-                                </v-flex>
-                            </v-layout>
-                        </v-container>
-                    </v-card>
+                    <perfil-card :user="user" @update="onUpdate"></perfil-card>
+                </v-flex>
+                <v-flex xs12 md4 sm8>
+                    <change-pass></change-pass>
                 </v-flex>
             </v-layout>
         </v-container>
@@ -43,6 +14,9 @@
 </template>
 
 <script>
+import PerfilCard from './PerfilCard.vue'
+import ChangePass from './ChangePass.vue'
+
 const api_config = require('@/api_config');
 
 export default {
@@ -50,14 +24,18 @@ export default {
         return {
             loading: false,
             user: {
-                // id: '';
-                // username: '',
-                // email: '',
-                // password: '',
-                // first_name: '',
-                // last_name: ''
+                id: 1,
+                username: 'teste',
+                email: 'teste',
+                password: 'teste',
+                first_name: 'teste',
+                last_name: 'teste'
             }
         }
+    },
+    components: {
+        PerfilCard,
+        ChangePass
     },
     beforeCreate: function () {
         if (!this.$session.exists()) {
@@ -68,32 +46,35 @@ export default {
         }
     },
     created: function () {
-        let self = this
-        this.$http.get(api_config.me_url)
-        .then(function(response) {
-            // console.log(response)
-            // JSON responses are automatically parsed.
-            self.user = response.data
-            console.log({'msg': 'user loaded!', 'user': self.user})
-        })
-        .catch(function(error) {
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                console.log({'data': error.response.data, 'status': error.response.status, 'headers': error.response.headers})
-            } else if (error.request) {
-                // The request was made but no response was received
-                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                // http.ClientRequest in node.js
-                console.log({'request': error.request})
-            } else {
-                // Something happened in setting up the request that triggered an Error
-                console.log({'Error': error.message})
-            }
-            console.log({'config': error.config});
-        })
+        // let self = this
+        // this.$http.get(api_config.me_url)
+        // .then(function(response) {
+        //     // console.log(response)
+        //     // JSON responses are automatically parsed.
+        //     self.user = response.data
+        //     console.log({'msg': 'user loaded!', 'user': self.user})
+        // })
+        // .catch(function(error) {
+        //     if (error.response) {
+        //         // The request was made and the server responded with a status code
+        //         // that falls out of the range of 2xx
+        //         console.log({'data': error.response.data, 'status': error.response.status, 'headers': error.response.headers})
+        //     } else if (error.request) {
+        //         // The request was made but no response was received
+        //         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        //         // http.ClientRequest in node.js
+        //         console.log({'request': error.request})
+        //     } else {
+        //         // Something happened in setting up the request that triggered an Error
+        //         console.log({'Error': error.message})
+        //     }
+        //     console.log({'config': error.config});
+        // })
     },
     methods: {
+        onUpdate (event) {
+            console.log(event)
+        },
         submit() {
             let self = this
             this.$http.patch(api_config+self.$session.get('id')+'/', self.user)
