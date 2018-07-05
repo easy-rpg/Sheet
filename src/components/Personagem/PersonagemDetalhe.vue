96d77279649af6a91ad50a9d49956642682ce355
RodrigoNdeC <rodrigondec@gmail.com>
2018-07-05 01:32:42 -0300

    Personagem 'funcionando'

--- src/components/Personagem/PersonagemDetalhe.vue
+++ src/components/Personagem/PersonagemDetalhe.vue
@@ -3,9 +3,9 @@
         <p>Loading...</p>
     </div>
     <div v-else>
-        <h1>Personagem X</h1>
+        <h1>{{ personagem.nome }}</h1>
         <hr>
-        <p>Info</p>
+        {{ personagem }}
     </div>
 </template>
 
@@ -31,29 +31,32 @@ export default {
     created: function () {
         this.loading = true
         let self = this
-        this.$http.get(api_config.personagem)
-        // .then(function(response) {
-        //     // console.log(response)
-        //     // JSON responses are automatically parsed.
-        //     self.user = response.data
-        //     console.log({'msg': 'user loaded!', 'user': self.user})
-        // })
-        // .catch(function(error) {
-        //     if (error.response) {
-        //         // The request was made and the server responded with a status code
-        //         // that falls out of the range of 2xx
-        //         console.log({'data': error.response.data, 'status': error.response.status, 'headers': error.response.headers})
-        //     } else if (error.request) {
-        //         // The request was made but no response was received
-        //         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        //         // http.ClientRequest in node.js
-        //         console.log({'request': error.request})
-        //     } else {
-        //         // Something happened in setting up the request that triggered an Error
-        //         console.log({'Error': error.message})
-        //     }
-        //     console.log({'config': error.config});
-        // })
+        this.$http.get(api_config.personagem+this.id+'/')
+        .then(function(response) {
+            // console.log(response)
+            // JSON responses are automatically parsed.
+            self.personagem = response.data
+            console.log({'msg': 'personagem loaded!', 'personagem': self.personagem})
+        })
+        .catch(function(error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log({'data': error.response.data, 'status': error.response.status, 'headers': error.response.headers})
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log({'request': error.request})
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log({'Error': error.message})
+            }
+            console.log({'config': error.config});
+        })
+        .then(function() {
+            self.loading = false
+        })
     },
     methods: {
         submit() {
