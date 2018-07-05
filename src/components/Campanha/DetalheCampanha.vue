7dacb25e361a0ac9b0da8d7f137854fe2e5180bc
RodrigoNdeC <rodrigondec@gmail.com>
2018-07-05 00:05:43 -0300

    minor fix

---
+++ src/components/Campanha/DetalheCampanha.vue
@@ -0,0 +1,81 @@
+<template>
+    <div>
+        <h1>Campanha X</h1>
+        <hr>
+        <p>Info</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return { }
+    },
+    props: ['campanha_id'],
+    beforeCreate: function () {
+        if (!this.$session.exists()) {
+            this.$router.push('/login')
+        }
+        else{
+            this.$http.defaults.headers.common['Authorization'] = 'Bearer ' + this.$session.get('jwt')
+        }
+    },
+    created: function () {
+        let self = this
+        this.$http.get(api_config.user_me)
+        .then(function(response) {
+            // console.log(response)
+            // JSON responses are automatically parsed.
+            self.user = response.data
+            console.log({'msg': 'user loaded!', 'user': self.user})
+        })
+        .catch(function(error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log({'data': error.response.data, 'status': error.response.status, 'headers': error.response.headers})
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log({'request': error.request})
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log({'Error': error.message})
+            }
+            console.log({'config': error.config});
+        })
+    },
+    methods: {
+        submit() {
+            let self = this
+            this.$http.patch(api_config.user+self.$session.get('id')+'/', self.user)
+            .then(function(response) {
+                console.log(response)
+                // JSON responses are automatically parsed.
+                // self.user = response.data
+                // console.log({'msg': 'user loaded!', 'user': self.user})
+            })
+            .catch(function(error) {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log({'data': error.response.data, 'status': error.response.status, 'headers': error.response.headers})
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                    // http.ClientRequest in node.js
+                    console.log({'request': error.request})
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log({'Error': error.message})
+                }
+                console.log({'config': error.config});
+            })
+        }
+    }
+}
+</script>
+
+<style>
+</style>
